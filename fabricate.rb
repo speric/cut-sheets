d629cb7920533bc13c8e66866e981e96997d191f
speric <eric@prudentiadigital.com>
2012-08-21 09:24:00 -0400

    Test for empty? instead of nil?

--- fabricate.rb
+++ fabricate.rb
@@ -1,30 +1,32 @@
 require 'csv'
 
-ISO_NUMBER 	= 0
-SIZE 		    = 1
-QUANTITY 	  = 2
-LENGTH 		  = 3
+ISO_NUMBER  = 0
+SIZE 		= 1
+QUANTITY 	= 2
+LENGTH 		= 3
 DESCRIPTION = 4
 
 fabrication_pieces_as_array = CSV.read("isos.csv")
 
-fabrication_pieces_as_array.delete_if { |x| x[ISO_NUMBER] == "" }
+fabrication_pieces_as_array.delete_if { |x| x[ISO_NUMBER].empty? }
 
 fabrication_pieces_as_array.each do |x|
   x[DESCRIPTION].gsub!(', 150LB, MALLEABLE IRON', '')
   x[DESCRIPTION].gsub!(', 150LB MALLEABLE IRON', '')
   x[DESCRIPTION].gsub!('API LINE PIPE COUPLING, THRD', 'API PIPE COUPLING, THRD')
   x[DESCRIPTION].gsub!('150LB MALLEABLE IRON, GRINNELL', '')
-  x[DESCRIPTION] = x[SIZE] + " " + x[DESCRIPTION] if x[LENGTH] == ""
+  x[DESCRIPTION] = x[SIZE] + " " + x[DESCRIPTION] if x[LENGTH].empty?
 end
 
 iso_pieces = fabrication_pieces_as_array.group_by{|row| row[ISO_NUMBER]}
 
 fabrication_pieces_as_array.each do |x|
-  x[DESCRIPTION] = iso_pieces[x[ISO_NUMBER]].select{ |y| y[LENGTH] == "" }.first[DESCRIPTION] if x[LENGTH] != "" and iso_pieces[x[ISO_NUMBER]].size == 2
+  if iso_pieces[x[ISO_NUMBER]].size == 2 and !x[LENGTH].nil? 
+    x[DESCRIPTION] = iso_pieces[x[ISO_NUMBER]].select{ |y| y[LENGTH].empty? }.first[DESCRIPTION]
+  end
 end
 
-fabrication_pieces_as_array.delete_if {|x| x[LENGTH] == "" and iso_pieces[x[ISO_NUMBER]].size == 2}.sort!{ |x, y| x[ISO_NUMBER] <=> y[ISO_NUMBER] }
+fabrication_pieces_as_array.delete_if {|x| x[LENGTH].empty? and iso_pieces[x[ISO_NUMBER]].size == 2}.sort!{ |x, y| x[ISO_NUMBER] <=> y[ISO_NUMBER] }
 
 CSV.open("cut-sheets.csv", "wb") do |csv|
   csv << ["ISO", "SIZE", "QUANTITY", "LENGTH", "DESCRIPTION"]
