6344001e62adfdefd0bfc4be6dfa80a2738158c2
speric <eric@prudentiadigital.com>
2012-08-20 21:25:27 -0400

    Working version 1. Parses 'isos.csv' and outputs 'cut-sheets.csv'

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ cut-sheets
 ==========
 
 Parse QuickPen-generated CSVs into useful fabrication cut sheets.
+
+**TODO**
+
+*  Error handling
+*  Take filename from cmd line
--- fabricate.rb
+++ fabricate.rb
@@ -1,23 +1,34 @@
 require 'csv'
 
 ISO_NUMBER 	= 0
-SIZE 		= 1
-QUANTITY 	= 2
-LENGTH 		= 3
+SIZE 		    = 1
+QUANTITY 	  = 2
+LENGTH 		  = 3
 DESCRIPTION = 4
 
-fabrication_pieces_as_array = CSV.read("test.csv")
+fabrication_pieces_as_array = CSV.read("isos.csv")
+
+fabrication_pieces_as_array.delete_if { |x| x[ISO_NUMBER] == "" }
+
+fabrication_pieces_as_array.each do |x|
+  x[DESCRIPTION].gsub!(', 150LB, MALLEABLE IRON','')
+  x[DESCRIPTION].gsub!(', 150LB MALLEABLE IRON','')
+  x[DESCRIPTION].gsub!('API LINE PIPE COUPLING, THRD','API PIPE COUPLING, THRD')
+  x[DESCRIPTION].gsub!('150LB MALLEABLE IRON, GRINNELL','')
+  x[DESCRIPTION] = x[SIZE] + " " + x[DESCRIPTION] if x[LENGTH] == ""
+end
+
+puts fabrication_pieces_as_array.inspect
 
 iso_pieces = fabrication_pieces_as_array.group_by{|row| row[ISO_NUMBER]}
 
-cut_sheets = Array.new
+fabrication_pieces_as_array.each do |x|
+  x[DESCRIPTION] = iso_pieces[x[ISO_NUMBER]].select{ |y| y[LENGTH] == "" }.first[DESCRIPTION] if x[LENGTH] != "" and iso_pieces[x[ISO_NUMBER]].size == 2
+end
 
-iso_pieces.each do |iso_num, pieces|
-  unless pieces.size > 2
-    hash = Hash[pieces.map.with_index{|*ki| ki}]
-    
-    puts hash
-    #hash['b'] # => 1
-  end
-  #puts "#{iso_num} \t=> #{pieces.size}"
+fabrication_pieces_as_array.delete_if {|x| x[LENGTH] == "" and iso_pieces[x[ISO_NUMBER]].size == 2}.sort!{ |x, y| x[ISO_NUMBER] <=> y[ISO_NUMBER] }
+
+CSV.open("cut-sheets.csv", "wb") do |csv|
+  csv << ["ISO", "SIZE", "QUANTITY", "LENGTH", "DESCRIPTION"]
+  fabrication_pieces_as_array.each { |x| csv << [x[ISO_NUMBER], x[SIZE], x[QUANTITY], x[LENGTH], x[DESCRIPTION]] }
 end
