e606248477bcbd322af1144721ed3573ec18b6a7
Eric Farkas <eric@prudentiadigital.com>
2013-05-21 15:13:04 -0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ end
 
 post '/parse' do
   if params[:csv_from_quickpen].nil?
-  	"Please upload a CSV"
+    "Please upload a CSV"
   else
-  	fabrication = QuickpenFabricator.new(import_file: params[:csv_from_quickpen][:tempfile].read)
+    fabrication = QuickpenFabricator.new(import_file: params[:csv_from_quickpen][:tempfile].read)
     headers "Content-Disposition" => "attachment;filename=cut-sheets.csv", "Content-Type" => "text/csv"
     fabrication.generate_cut_sheets_csv_data
   end
