34e6582e5e0d94a673678aab8b614ffa0b20cafa
speric <eric@prudentiadigital.com>
2013-05-21 13:39:26 -0400

    Sinatra app, working version.

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
+require 'csv'
+require './lib/quickpen_fabricator'
 
-class CutSheets < Sinatra::Base
-  get '/' do
-    erb :index
+get '/' do
+  erb :index
+end
+
+post '/parse' do
+  if params[:csv_from_quickpen].nil?
+  	"Please upload a CSV"
+  else
+  	fabrication = QuickpenFabricator.new(import_file: params[:csv_from_quickpen][:tempfile].read)
+    headers "Content-Disposition" => "attachment;filename=cut-sheets.csv", "Content-Type" => "text/csv"
+    fabrication.generate_cut_sheets_csv_data
   end
-end 
+end
---
+++ lib/quickpen_fabricator.rb
@@ -0,0 +1,59 @@
+class Object
+  def blank?
+    self.length == 1
+  end
+end
+
+class QuickpenFabricator
+  ISO_NUMBER  = 0
+  QUANTITY    = 1
+  SIZE        = 2
+  LENGTH      = 3
+  DESCRIPTION = 4
+  
+  def initialize(attributes={})
+    @import_file = attributes[:import_file]
+  end
+
+  def generate_cut_sheets_csv_data
+    @fabrication_pieces_as_array = CSV.parse(@import_file, headers: true)
+    @fabrication_pieces_as_array.delete_if { |x| x[ISO_NUMBER].blank? }
+    
+    clean_up_descriptions
+    combine_cut_pipe_and_makeup_fittings
+    generate_csv
+  end
+
+  private
+  
+  def clean_up_descriptions
+    @fabrication_pieces_as_array.each do |piece|
+      piece[DESCRIPTION].gsub!(', 150LB, MALLEABLE IRON', '')
+      piece[DESCRIPTION].gsub!(', 150LB MALLEABLE IRON', '')
+      piece[DESCRIPTION].gsub!('API LINE PIPE COUPLING, THRD', 'API PIPE COUPLING, THRD')
+      piece[DESCRIPTION].gsub!('150LB MALLEABLE IRON, GRINNELL', '')
+      piece[SIZE] = piece[SIZE] + '"'
+    end
+  end
+
+  def combine_cut_pipe_and_makeup_fittings
+    @iso_pieces = @fabrication_pieces_as_array.group_by{|row| row[ISO_NUMBER]}
+
+    @fabrication_pieces_as_array.each do |piece|
+      if @iso_pieces[piece[ISO_NUMBER]].size == 2 and !piece[LENGTH].blank?
+        piece[DESCRIPTION] = @iso_pieces[piece[ISO_NUMBER]].select{ |y| y[LENGTH].blank? }.first[DESCRIPTION]
+      end
+    end
+
+    @fabrication_pieces_as_array.delete_if {|piece| piece[LENGTH].blank? and @iso_pieces[piece[ISO_NUMBER]].size == 2}
+  end
+
+  def generate_csv
+    csv_content = CSV.generate do |csv|
+      csv << ["ISO", "SIZE", "QUANTITY", "LENGTH", "DESCRIPTION"]
+      @fabrication_pieces_as_array.each { |x| csv << [x[ISO_NUMBER], x[SIZE], x[QUANTITY], x[LENGTH], x[DESCRIPTION]] }
+      @fabrication_pieces_as_array.group_by { |x| x[DESCRIPTION] }.each { |key, values| csv << ["", "", values.size, "", key] }
+    end
+    csv_content
+  end
+end
